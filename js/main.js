17f4379db4337d54da480fb2d5ee5fe0c61aa78c
임진영 <delheure29@gmail.com>
2021-01-26 19:33:09 +0900

    Add: first commit

--- js/main.js
+++ js/main.js
@@ -2,32 +2,41 @@ const btnTxtMore = document.querySelector(".btn_txt_more");
 const feedTxt = document.querySelector(".feeds_txt p");
 
 btnTxtMore.addEventListener("click", () => {
-    feedTxt.classList.add("all");
-    btnTxtMore.remove();
+  feedTxt.classList.add("all");
+  btnTxtMore.remove();
 });
 
 const likes = document.querySelectorAll(".btn_heart");
+const likeCount = document.querySelector(".like_count span");
+
 for (const like of likes) {
-    like.addEventListener("click", function () {
-        this.classList.toggle("on");
-    });
+  let num = 0;
+  like.addEventListener("click", function () {
+    this.classList.toggle("on");
+    num += 1;
+    likeCount.innerHTML = `${num}개`;
+  });
 }
 
 const cmtBox = document.querySelector(".cmt_list");
 const cmtInput = document.querySelector(".cmt_write input");
 const cmtBtn = document.querySelector(".btn_submit");
 
-cmtBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (!cmtInput.value) {
-        return;
-    }
-    cmtBox.innerHTML = `<li>
+cmtBtn.addEventListener("click", e => {
+  e.preventDefault();
+  if (!cmtInput.value) {
+    return;
+  } else {
+    cmtBox.insertAdjacentHTML(
+      "afterbegin",
+      `<li>
         <p>
             <strong class="user_id">${"delheure"}</strong>
             ${cmtInput.value}
         </p>
         <button class="btn_heart btn_small btn_com">Like</button>
-    </li>`;
+    </li>`
+    );
     cmtInput.value = "";
+  }
 });
