017b78cf62050202496c798bdcdc290a4a4b01cf
임진영 <delheure@gmail.com>
2021-02-04 16:36:54 +0900

    Add : 댓글 삭제, 좋아요 기능 추가

---
+++ js/index.js
@@ -0,0 +1,72 @@
+const btnTxtMore = document.querySelector(".btn_txt_more");
+const feedTxt = document.querySelector(".feeds_txt p");
+
+btnTxtMore.addEventListener("click", () => {
+  feedTxt.classList.add("all");
+  btnTxtMore.remove();
+});
+
+const likes = document.querySelectorAll(".btn_heart");
+const likeCount = document.querySelector(".like_count span");
+
+for (const like of likes) {
+  let num = 0;
+  like.addEventListener("click", function () {
+    this.classList.toggle("on");
+    if (this.classList.contains("on")) {
+      this.innerHTML = `<svg aria-label="좋아요 취소" class="_8-yf5 " fill="#ed4956" height="24" viewBox="0 0 48 48" width="24"><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>`;
+      num++;
+    } else {
+      this.innerHTML = `<svg aria-label="좋아요" class="_8-yf5 " fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>`;
+      num--;
+    }
+    likeCount.innerHTML = `${num}개`;
+  });
+}
+
+const cmtBox = document.querySelector(".cmt_list");
+const cmtInput = document.querySelector(".cmt_write input");
+const cmtBtn = document.querySelector(".btn_submit");
+ 
+const addCmt = (e) => {
+  e.preventDefault();
+  if (!cmtInput.value) {
+    return;
+  }
+    const cmt = document.createElement("li");
+    
+    const cmtWr = document.createElement("p");
+
+    const cmtId = document.createElement("strong");
+    cmtId.setAttribute('class', "user_id");
+    cmtId.innerHTML = `${"delheure"}`;
+
+    const cmtText = document.createElement("span");
+    cmtText.innerHTML = `${cmtInput.value}`;
+
+    const cmtDel = document.createElement("button");
+    cmtDel.setAttribute('class', 'btn_del');
+    cmtDel.innerHTML = "X";
+    cmtDel.addEventListener("click", () => {
+      cmtBox.removeChild(cmt);
+    });
+
+    const cmtLike = document.createElement("button");
+    cmtLike.setAttribute('class', 'btn_like');
+    cmtLike.innerHTML = `<svg aria-label="좋아요" class="_8-yf5 " fill="#262626" height="12" viewBox="0 0 48 48" width="12"><path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>`;
+    cmtLike.addEventListener("click", () => {
+      cmtLike.setAttribute('class', 'btn_like on');
+      cmtLike.innerHTML = `<svg aria-label="좋아요 취소" class="_8-yf5 " fill="#ed4956" height="12" viewBox="0 0 48 48" width="12"><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>`;
+    });
+
+    cmt.appendChild(cmtWr);
+    cmtWr.appendChild(cmtId);
+    cmtWr.appendChild(cmtText);
+    cmt.appendChild(cmtDel);
+    cmt.appendChild(cmtLike);
+    cmtBox.appendChild(cmt);
+
+    cmtInput.value = "";  
+}
+
+cmtBtn.addEventListener("click", addCmt);
